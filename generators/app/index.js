ffabc98315e7b509f545fc440f7790ef0cb95f0b
iWuzhi <2461358597@qq.com>
2020-08-30 07:41:59 +0800

    Add download template basic support

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,37 +2,35 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const download = require('download-git-repo');
+const ora = require('ora');
 
 module.exports = class extends Generator {
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(`Welcome to the ultimate ${chalk.red('generator-mr-cli')} generator!`)
     );
+  }
 
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+  async writing() {
+    const spinner = ora('Download template from github, it may be need a little time, please wait.').start();
+    return new Promise((resolve, reject) => {
+      download('github:micro-react/app-template', this.destinationRoot(), {}, function (err) {
+        if (err) {
+          spinner.fail()
+          reject(err);
+        } else {
+          spinner.succeed()
+          resolve();
+        }
+      });
     });
   }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
-
   install() {
-    this.installDependencies();
+    this.yarnInstall();
   }
+  
 };
